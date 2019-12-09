ad83df6aed7465fb4a76b62a0511f38e5012e2b4
bert <1131482051@qq.com>
2019-12-09 13:55:34 +0800

    12.9update
    完成下载器部分代码，修复图片下载部分损坏问题

--- ImagePoolProject/DatabaseQuery.go
+++ ImagePoolProject/DatabaseQuery.go
@@ -3,16 +3,37 @@ import (
 	"fmt"
 	"github.com/astaxie/beego/orm"
 )
-
-func init()  {
+var O orm.Ormer
+func init(){
+	orm.RegisterDataBase("default", "mysql", "root:116118@tcp(www.whatdoyoudo.club:3306)/dataset?charset=utf8", 30)
+	// create table
+	orm.RegisterModel(new(ModelInit))
+	orm.RunSyncdb("default", false, true)
+	O=orm.NewOrm()
 }
 
-func GroupUrlGet(Group string){
-	o:=orm.NewOrm()
-	var obj []interface{}
-	_,err:=o.Raw("select imageID from imageLabelTable where labelName = ? and confidence=1 limit 10;",Group).QueryRows(&obj)
+func GroupImageIDGet(Group string)[]orm.ParamsList{
+	var res  []orm.ParamsList
+	_,err:=O.Raw("select distinct imageID from imageLabelTable where labelName = ? and confidence=1 limit 10;",Group).ValuesList(&res)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%#v",obj)
+	return res
+}
+type UrlType struct {
+	ImageID string
+	Url string
+}
+func GroupUrlGet(GroupList []orm.ParamsList){
+
+	var res orm.ParamsList
+	fmt.Printf("%#v",len(GroupList))
+	for i:=0;i<len(GroupList);i++{
+		fmt.Printf("%s",GroupList[i][0])
+		_,err:=O.Raw("select urlLink from urlTable where imageID=?",GroupList[i][0]).ValuesFlat(&res)
+		if err != nil {
+			fmt.Printf("%#v",err)
+		}
+		fmt.Printf("%#v",res)
+	}
 }
